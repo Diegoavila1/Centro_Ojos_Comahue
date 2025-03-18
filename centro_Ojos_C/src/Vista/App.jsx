bc5c5a4b15cd955135b55e5f7db0aac030ff4b04
Diegoavila1 <diegoavilaaa0@gmail.com>
2025-03-18 20:18:26 -0300

    -instalacion de react -Instalacion de tailwind -creacion MCV

---
+++ centro_Ojos_C/src/Vista/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-center bg-red-200'>Centro de ojos</p>
+    </>
+  )
+}
+
+export default App
